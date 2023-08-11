6886eb853b04446e4fa5ce8733cc497eca5fbe82
Giovanni Cherubin <gcherubin@microsoft.com>
2023-08-11 11:08:29 +0100

    Update paths for scripts

--- submission-code/real-data-experiments/timing-experiments.sh
+++ submission-code/real-data-experiments/timing-experiments.sh
@@ -1,5 +1,7 @@
 outdir=results/timing/
 
+mkdir -p $outdir
+
 python train_bayes_sgd.py --device cuda:3 --dataset adult --batch-size 256 --epochs 10 --lr 0.15 --max-per-sample-grad_norm 1 --disable-dp --log $outdir/adult-disable-dp.jsonl
 python train_bayes_sgd.py --device cuda:3 --dataset adult --batch-size 256 --epochs 10 --lr 0.15 --max-per-sample-grad_norm 1 --bayes-mia --log $outdir/adult-mia.jsonl
 python train_bayes_sgd.py --device cuda:3 --dataset adult --batch-size 256 --epochs 10 --lr 0.15 --max-per-sample-grad_norm 1 --moments-accountant --log $outdir/adult-accountant.jsonl
